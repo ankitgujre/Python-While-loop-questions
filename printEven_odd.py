1a5719732e144620410c102b474f6ca9a89bcaa6
Ankit Gujre <ankitgujre374@gmail.com>
2026-03-20 08:59:20 +0530

    print even odd

---
+++ printEven_odd.py
@@ -0,0 +1,19 @@
+'''
+3. Print all even numbers between 1 and 100. 
+'''
+# num = 1
+# while num <= 100:
+#     if(num % 2 == 0):
+#         print("Even = ", num)
+#     num += 1
+
+
+'''
+4. Print all odd numbers between 1 and 100.
+'''
+
+num = 1
+while num <= 100:
+    if num % 2 != 0:
+        print("Odd =", num)
+    num += 1
