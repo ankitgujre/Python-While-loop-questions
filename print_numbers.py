c06d285a39c72c7b146da7c79a02d6c8051e2ef2
Ankit Gujre <ankitgujre374@gmail.com>
2026-03-20 08:51:43 +0530

    print numbers 1-10 and 10-1

---
+++ print_numbers.py
@@ -0,0 +1,17 @@
+'''
+wap to print numbers from 1 to 10
+'''
+
+# num = 1
+# while num <= 10:
+#     # print(num)
+#     print(num, end=" ")
+#     num += 1
+     
+'''
+2. Print numbers from 10 down to 1 in reverse order.
+'''
+num = 10
+while num >= 1:
+    print(num)
+    num -= 1
